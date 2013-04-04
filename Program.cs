12bdb5797c93ecf07bb960f371635e7f4c5aaa53
janez <janez@gmail.com>
2013-04-04 09:44:02 +0200

    jk
    Signed-off-by: janez <janez@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using LXComm;
+
+namespace LXF
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [MTAThread]
+        static void Main()
+        {
+
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            try
+            {
+                Application.Run(new Form_main());
+            }
+            catch (Exception napaka)
+            {
+
+                int x = 5;
+            }
+           
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception napaka = (Exception)e.ExceptionObject;
+            if (napaka != null)
+            {
+                StreamWriter sw = new StreamWriter(@"\"+Root.Instance.Path+@"\log.txt", true);
+              
+                sw.WriteLine("///////////////////////////////////////////////////////////////");
+                sw.Write(napaka.Message);
+                sw.WriteLine("---------------->");
+                sw.Write(napaka.ToString());
+                sw.Close();
+                LXComm.Remote.WriteLastShutDownTime(LXComm.eReconnectType.UNHANDLED_EXCEPTION);
+                System.Threading.Thread.Sleep(4000);
+                System.Diagnostics.Process.Start(@"\Windows\D.exe", "");
+
+            }
+
+
+
+        }
+    }
+}
